bee11cc23105ba62527d439c596ec0d94561d2fe
Sagleft <sagleft@mfc.click>
2022-02-18 01:18:43 +0200

    setup telegram connection

--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"errors"
 	"log"
+	"time"
 
 	utopiago "github.com/Sagleft/utopialib-go"
 	"github.com/gin-gonic/gin"
+	"gopkg.in/tucnak/telebot.v2"
 )
 
 func main() {
@@ -58,6 +60,17 @@ func (sol *solution) connectUtopia() error {
 }
 
 func (sol *solution) connectTelegram() error {
-	// TODO
+	if sol.Config.Telegram.Token == "" {
+		return errors.New("telegram token is not set in " + configJSONPath)
+	}
+
+	var err error
+	sol.Messengers.Telegram, err = telebot.NewBot(telebot.Settings{
+		Token:  sol.Config.Telegram.Token,
+		Poller: &telebot.LongPoller{Timeout: 10 * time.Second},
+	})
+	if err != nil {
+		return errors.New("failed to connect to telegram bot: " + err.Error())
+	}
 	return nil
 }
